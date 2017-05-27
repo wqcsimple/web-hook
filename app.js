9d0efb47759be1c321a222a474738ce948d6c92b
whis <wqcsimple@foxmail.com>
2017-05-27 16:15:55 +0800

    add return log function

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(router);
 app.use(Core.errorHandler);
 app.use(Core.notFoundHandler);
@@ -15,17 +15,35 @@ app.use(Core.notFoundHandler);
 function execute(context, command) {
     Log.i(command);
     Util.execute(command)
-        .then(() => { context.finish('done') })
-        .catch(e => { context.finish({ error: Util.getFormattedJson(e) }) })
+        .then(() => {
+            context.finish('done')
+        })
+        .catch(e => {
+            context.finish({error: Util.getFormattedJson(e)})
+        })
     ;
 }
 
+function executeAndReturn(context, command) {
+    Util.execute(command)
+        .then((res) => {
+            let template = require('./response');
+            let resData = res.split("\n").reverse().join('<br />');
+
+            context.response.send(template(resData));
+        })
+        .catch(e => {
+            context.finish({error: Util.getFormattedJson(e)})
+        })
+}
+
 var api = {
-    '~/launch/boot-test-api':                            [context => execute(context, '/opt/script/boot_test_api')],
-    '~/launch/boot-gh-api':                            [context => execute(context, '/opt/script/boot_gh_web')],
-    '~/launch/boot-video-view-web':                            [context => execute(context, '/opt/script/boot_video_view_web')],
-    '~/launch/boot-lexiang-web':                            [context => execute(context, '/opt/script/boot_lexiang_web')],
-    '~/launch/boot_whis_blog':                            [context => execute(context, '/opt/script/boot_whis_blog')],
+    '~/launch/boot-test-api': [context => execute(context, '/opt/script/boot_test_api')],
+    '~/launch/boot-gh-api': [context => execute(context, '/opt/script/boot_gh_web')],
+    '~/launch/boot-video-view-web': [context => execute(context, '/opt/script/boot_video_view_web')],
+    '~/launch/boot-lexiang-web': [context => execute(context, '/opt/script/boot_lexiang_web')],
+    '~/launch/boot_whis_blog': [context => execute(context, '/opt/script/boot_whis_blog')],
+    '~/launch/test': [context => executeAndReturn(context, 'ls /Users/whis/Workspace/whis')],
 };
 
 Core.install(router, api);
---
+++ response.js
@@ -0,0 +1,36 @@
+/**
+ * @author whis admin@wwhis.com
+ * @Created 5/27/17
+ */
+module.exports = data => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Log</title>
+    <style type="text/css">
+    * {
+        margin: 0;
+        padding: 0;
+        font-size: 14px;
+        box-sizing: border-box;
+        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+        -webkit-touch-callout: none;
+        //-webkit-appearance: none;
+        -webkit-font-smoothing: antialiased;
+        font-size: 13px; line-height: 20px;
+        background: #222; color: #ddd;
+        font-family: "Consolas", "Monaco", "Menlo";
+    }
+    body {
+     padding: 0 15px;
+    }
+    </style>
+</head>
+
+<body>
+<div>${data}</div>
+</body>
+
+</html>
+`
